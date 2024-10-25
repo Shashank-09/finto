720f779c0930f78b48cef425e394d2ea9661a2ae
Shashank Tulsikar <shashanktulsikar979@gmail.com>
2024-10-25 15:26:49 +0530

    layout setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hreo from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
-      
+    <Hreo />
     </>
   );
 }
