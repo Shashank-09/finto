5d58146a89999b6003a4ee68818c1aea754a11ea
Shashank Tulsikar <shashanktulsikar979@gmail.com>
2024-10-26 19:22:02 +0530

    update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,27 @@ import styles from "./page.module.css";
 import Hreo from "@/components/Hero";
 import About from "@/components/About";
 import AboutCard from "@/components/AboutCard";
+import Revenue from "@/components/Revenue";
+import Partner from "@/components/Partner";
+import Services from "@/components/Services";
+import Financial from "@/components/Financial";
+import StartWork from "@/components/StartWork";
+import Pricing from "@/components/Pricing";
+import Blog from "@/components/Blog";
 
 export default function Home() {
   return (
     <>
     <Hreo />
+    <Revenue />
+    {/* <Partner /> */}
     <About/>
-  <AboutCard/>
+    <AboutCard/>
+    <Services />
+    <Financial />
+    <StartWork />
+    <Pricing />
+    <Blog />
     </>
   );
 }
