8bf8a4f5e53ee4b5001f29cd28ad699bdc36dfae
Shashank Tulsikar <shashanktulsikar979@gmail.com>
2024-10-26 19:21:43 +0530

    Start Work component

---
+++ src/components/StartWork/index.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+
+const StartWork = () => {
+  return (
+    <section>
+          <div className="works-area bg-color-0c3a30 ptb-120">
+            <div className="container">
+                <div className="about-top mb-5">
+                    <div className="row align-items-center">
+                        <div className="col-lg-7 col-md-7" data-cues="slideInRight" data-duration="800">
+                            <div className="section-heading mb-0">
+                                <span className="sub-title two bg-color-9edd05 rounded-pill">HOW IT WORKS</span>
+                                <h2 className="text-white mb-0">Get Started And Enjoy Full Service Features</h2>
+                            </div>
+                        </div>
+                        <div className="col-lg-5 col-md-5" data-cues="slideInLeft" data-duration="800">
+                            <div className="content">
+                                <p className="text-white">By integrating advanced technology with financial expertise we provide a comprehensive suite of services that cater to both individuals and businesses</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row g-0 mb-5">
+                    <div className="col-lg-3 col-md-3">
+                        <div className="step-card position-relative">
+                            <h3 className="text-white">Step One</h3>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-3">
+                        <div className="step-card position-relative">
+                            <h3 className="text-white">Step Two</h3>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-3">
+                        <div className="step-card position-relative">
+                            <h3 className="text-white">Step Three</h3>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-3">
+                        <div className="step-card position-relative">
+                            <h3 className="text-white">Step Four</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row g-4" data-cues="slideInUp" data-duration="800">
+                    <div className="col-xl-3 col-lg-6 col-md-6">
+                        <div className="works-card bg-color-29594b radius-30">
+                            <i className="flaticon-coins-2"></i>
+                            <h3 className="text-white">Create Account</h3>
+                            <p className="text-white">Our mission is to bridge the gap between traditional banking modern offering innovative seamless</p>
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-lg-6 col-md-6">
+                        <div className="works-card bg-color-29594b radius-30">
+                            <i className="flaticon-businessman-6"></i>
+                            <h3 className="text-white">User Confirmation</h3>
+                            <p className="text-white">Our mission is to bridge the gap between traditional banking modern offering innovative seamless</p>
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-lg-6 col-md-6">
+                        <div className="works-card bg-color-29594b radius-30">
+                            <i className="flaticon-laptop-1"></i>
+                            <h3 className="text-white">Enjoy Full Access</h3>
+                            <p className="text-white">Our mission is to bridge the gap between traditional banking modern offering innovative seamless</p>
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-lg-6 col-md-6">
+                        <div className="works-card bg-color-29594b radius-30">
+                            <i className="flaticon-dollar-bill"></i>
+                            <h3 className="text-white">Mobile Transaction</h3>
+                            <p className="text-white">Our mission is to bridge the gap between traditional banking modern offering innovative seamless</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default StartWork
